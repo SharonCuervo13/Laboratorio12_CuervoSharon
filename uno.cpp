aa96b18720c22b6254582b7d7a2019fce61db6dd
SharonCuervo13 <56890913+SharonCuervo13@users.noreply.github.com>
2019-10-30 18:28:33 -0500

    Delete uno.cpp

--- uno.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int main(){
-    float r;
-    std::cout <<"Ingrese el radio del circulo"<<std::endl;
-    std::cin>>r;
-    std::cout <<"diametro:"<<2*r<<std::endl;
-    std::cout <<"perimetro:"<<2*3.141592*r<<std::endl;
-    std::cout <<"area:"<<3.141592*r*r<<std::endl;
-}
